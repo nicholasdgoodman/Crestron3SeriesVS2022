8d8d0e7a9b845f3af45365ad4fc714eed437d2af
Nicholas Goodman <nicholasdgoodman@users.noreply.github.com>
2022-10-11 19:38:59 -0400

    Add project files.

---
+++ AssemblySign/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using CSharpCompiler;
+
+namespace AssemblySign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var signer = new SignAssembly();
+
+            foreach(var filename in args)
+            {
+                try
+                {
+                    signer.Sign(filename);
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine("Unable to sign file {0}\n{1})", filename, ex.Message);
+                }
+            }
+        }
+    }
+}
